a8cb659aab767e9468b516ef61612abe7a0230f3
EdS <Sousaa.dev@gmail.com.br>
2025-06-15 22:49:57 -0300

    atualizações no package json e criação teste do servidor

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,11 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const port = 8800
+
+app.get('/', (req, res) => {
+  res.send('Eduardo')
+})
+
+app.listen(port, () => {
+  console.log(`Exemplo de app listado na porta ${port}`)
+})
